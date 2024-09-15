e24bdd6143285810b419a1c86c3f50e1fa21b9d0
Hồ Chí Tài <hochitai24@gmail.com>
2024-09-15 22:25:07 +0700

    MUN-20 create entities

---
+++ MunShopApplication/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MunShopApplication.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
